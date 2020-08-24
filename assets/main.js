8e3e77d4b9f29824d631af40b55146c79937f6d2
servedois <servedois@gmail.com>
2020-08-24 12:27:19 +0100

    update page variable

--- assets/main.js
+++ assets/main.js
@@ -8,7 +8,7 @@
 
 
     $("#comment-form-submit").html(
-      '<svg class="icon spin"><use xlink:href="#icon-loading"></use></svg> Sending...'
+      '<svg class="icon spin"><use xlink:href="#icon-loading"></use></svg> Enviando...'
     );
     $(form).addClass('disabled');
 
